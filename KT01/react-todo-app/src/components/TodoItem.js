6936c00adcd86233b4e04e85349387de2093e4ce
THISURA97 <tdinith481@gmail.com>
2021-12-09 18:14:36 +0530

    update kt content

---
+++ KT01/react-todo-app/src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React from "react"
+import React from "react"
+
+function TodoItem(props) {
+  return (
+  <li>
+      <input type="checkbox" />
+      {this.props.todo.title}
+  </li>
+  )
+}
+
+export default TodoItem
+
+
